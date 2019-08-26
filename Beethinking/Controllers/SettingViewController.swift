b57bbc6399d0998e31a816e287a166d135e8fb56
堀川浩二 <>
2019-08-26 13:24:35 +0800

    全データ消去で出る不具合

--- Beethinking/Controllers/SettingViewController.swift
+++ Beethinking/Controllers/SettingViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RealmSwift
+import SCLAlertView
 
 class SettingViewController: UIViewController {
 
@@ -14,6 +16,37 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func didTapTextCreateNew(_ sender: UITapGestureRecognizer) {
+        allDeleatData()
+    }
+    @IBAction func didTapIconCreateNew(_ sender: UITapGestureRecognizer) {
+        allDeleatData()
+    }
+    
+    func allDeleatData() {
+        
+        let alertView = SCLAlertView()
+        alertView.addButton("はい") {
+                    // (1)Realmインスタンスの生成
+                    let realm = try! Realm()
+                    // (2)全データの削除
+                    try! realm.write {
+                        realm.deleteAll()
+                    }
+        }
+        alertView.showInfo(
+            "すべてのデータが\n削除されます", // タイトル
+            subTitle: "ほんとうに削除しますか", // サブタイトル
+            closeButtonTitle: "いいえ", // クローズボタンのタイトル
+            //                timeout: 2 , // **秒ごに、自動的に閉じる（OKでも閉じることはできる）
+            colorStyle: 0xFF2600, // ボタン、シンボルの色
+            colorTextButton: 0x000000, // ボタンの文字列の色
+            //            circleIconImage: UIImage?, //アイコンimage
+            animationStyle:.bottomToTop // スタイル（Success)指定
+        )
     }
     
 
